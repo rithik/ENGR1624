3aaa9ab82c644cb04687fb863c5482e057d7283e
tim_the_legend <tim_the_legend@gmail.com>
2018-11-29 22:55:40 -0500

    Old Version of code reverted

--- detector.py
+++ detector.py
@@ -3,11 +3,15 @@ import cv2
 from scipy.spatial import distance
 from imutils import face_utils
 import time
+import datetime
+import math
 
 FRAMES = 10
+t1 = datetime.datetime.now()
+delaytime = 3
 
 class Detector():
-    def __init__(self, eyesNotVisibleTime=FRAMES, frame_check_time=FRAMES):
+    def __init__(self, eyesNotVisibleTime=t1, frame_check_time=t1):
         self.eyesNotVisible = 0
         self.flag = 0
         self.tEyesNotVisible = eyesNotVisibleTime
@@ -32,12 +36,32 @@ class Detector():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         subjects = self.detect(gray, 0)
         if len(subjects) == 0:
-            self.eyesNotVisible+=1
-            if self.eyesNotVisible >= self.tEyesNotVisible: # Distracted checker
+            self.eyesNotVisible=datetime.datetime.now()
+            if (self.eyesNotVisible-self.tEyesNotVisible).seconds >= delaytime: # Distracted checker
                 return True
         else:
-            self.eyesNotVisible = 0
-        return False
+            self.tEyesNotVisible=datetime.datetime.now()
+        print(len(subjects)) 
+        for subject in subjects:
+            shape=self.predict(gray,subject)
+            shape=face_utils.shape_to_np(shape)#converting to NumPy Array
+            leftEye=shape[self.lStart:self.lEnd]
+            rightEye=shape[self.rStart:self.rEnd]
+            leftEAR=self.eye_aspect_ratio(leftEye)
+            rightEAR=self.eye_aspect_ratio(rightEye)
+            ear=(leftEAR+rightEAR)/2.0
+            leftEyeHull=cv2.convexHull(leftEye)
+            rightEyeHull=cv2.convexHull(rightEye)
+            if drawing:
+                cv2.drawContours(frame,[leftEyeHull],-1,(0,255,0),1)
+                cv2.drawContours(frame,[rightEyeHull],-1,(0,255,0),1)
+            if ear < self.thresh: # Drowsiness Detector
+                self.flag+=1
+               	if self.flag>=self.frame_check:
+                    return True
+                else:
+                    self.flag=0
+                    return False
 
     def display_warnings(self, frame):
         cv2.putText(frame, "****************DISTRACTED!****************", (10, 30),
